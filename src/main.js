526b2dacb2f530b3e0bd5ce2e4ea859d3c6711be
Surajjberaa <berasuraj08@gmail.com>
2025-04-19 07:49:07 +0530

    Added next section with animation

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import vertexShader from './shaders/vertexShader.glsl';
 import fragmentShader from './shaders/fragmentShader.glsl';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/src/ScrollTrigger';
+import Lenis from 'lenis';
 
 let scene, camera, renderer, geometry, material, mesh, canvas;
 
@@ -46,6 +47,23 @@ window.addEventListener('resize', function() {
   camera.updateProjectionMatrix();
 });
 
+//Lenis
+
+// Initialize a new Lenis instance for smooth scrolling
+const lenis = new Lenis();
+
+// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+lenis.on('scroll', ScrollTrigger.update);
+
+// Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+// This ensures Lenis's smooth scroll animation updates on each GSAP tick
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+});
+
+// Disable lag smoothing in GSAP to prevent any delay in scroll animations
+gsap.ticker.lagSmoothing(0);
+
 
 var tl = gsap.timeline({
   scrollTrigger: {
@@ -74,10 +92,118 @@ tl.to(mesh.position, {
 .to('.landing p', {
   opacity: 1,
 })
-.to('body', {
+.to('.bg-overlay', {
   background: '#FAE1EE',
-  ease: "power2.inOut",
+  ease: 'power4.inOut',
   duration: 1
+})
+.to('.theBox', {
+  scrollTrigger: {
+    trigger: '.theBox',
+    start: 'center 50%',
+    end: '720% center',
+    pin: true,
+    pinSpacing: false,
+    // markers: true,
+    scrub: 1,
+    // markers: true
+  }
+}, )
+.to('.img1', {
+  y: '-=100vh',
+  duration: 1,
+  ease: 'power1.inOut',
+  scrollTrigger: {
+    trigger: '.leftDiv1',
+    start: '55% 50%',
+    end: '150% center',
+    scrub: 1,
+    // markers: true
+  }
+}, 'change1' )
+.to('.img2', {
+  y: '-=100vh',
+  duration: 2,
+  ease: 'power1.inOut',
+  scrollTrigger: {
+    trigger: '.leftDiv2',
+    start: '55% 50%',
+    end: '150% center',
+    scrub: 1,
+    // markers: true
+  }
+}, 'change2')
+.to('.img3', {
+  y: '-=100vh',
+  duration: 2,
+  ease: 'power1.inOut',
+  scrollTrigger: {
+    trigger: '.leftDiv3',
+    start: '45% 50%',
+    end: '150% center',
+    scrub: 1,
+    // markers: true
+  }
+}, 'change3')
+.to('.img4', {
+  y: '-=100vh',
+  duration: 2,
+  ease: 'power1.inOut',
+  scrollTrigger: {
+    trigger: '.leftDiv4',
+    start: '45% 50%',
+    end: '150% center',
+    scrub: 1,
+    // markers: true
+  }
+}, )
+.to('.bg-overlay', {
+  background: '#E0F0FF',
+  ease: 'power2.inOut',
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.leftDiv1',
+    start: '55% 50%',
+    end: '150% center',
+    scrub: 1,
+    // markers: true
+  }
+})
+.to('.bg-overlay', {
+  background: '#FFEDE0',
+  ease: 'power2.inOut',
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.leftDiv2',
+    start: '55% 50%',
+    end: '150% center',
+    scrub: 1,
+    // markers: true
+  }
+})
+.to('.bg-overlay', {
+  background: '#D3D6F0',
+  ease: 'power2.inOut',
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.leftDiv3',
+    start: '45% 50%',
+    end: '150% center',
+    scrub: 1,
+    // markers: true
+  }
+})
+.to('.bg-overlay', {
+  background: '#FFEAE0',
+  ease: 'power2.inOut',
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.leftDiv4',
+    start: '45% 50%',
+    end: '150% center',
+    scrub: 1,
+    // markers: true
+  }
 })
 
 
